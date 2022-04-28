387793f5f89c1c2eee9deaa1f5eb01a883bf5b79
João Inácio <egnawake@gmail.com>
2022-04-28 21:56:21 +0100

    Override Health on MilitaryUnit

--- GameUnits/MilitaryUnit.cs
+++ GameUnits/MilitaryUnit.cs
@@ -6,6 +6,7 @@ namespace GameUnits
     {
         public int AttackPower { get; }
         public int XP { get; set; }
+        public override int Health => base.Health + XP;
         public override float Value => AttackPower + XP;
 
         public MilitaryUnit(int movement, int health, int power) : base(movement, health)
