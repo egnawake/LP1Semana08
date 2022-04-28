fcabe56ae29bac7e8e7b3de70b3b1428c7d206bf
João Inácio <egnawake@gmail.com>
2022-04-28 22:05:39 +0100

    Test Unit subclasses

--- GameUnits/Program.cs
+++ GameUnits/Program.cs
@@ -6,7 +6,19 @@ namespace GameUnits
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Warrior");
+            MilitaryUnit warrior = new MilitaryUnit(3, 25, 5);
+            warrior.Move();
+            Console.WriteLine($"Health: {warrior.Health}");
+            Console.WriteLine($"Value: {warrior.Value}");
+            Console.WriteLine("");
+
+            Console.WriteLine("Settler");
+            SettlerUnit settler = new SettlerUnit(2, 15);
+            settler.Move();
+            Console.WriteLine($"Health: {settler.Health}");
+            Console.WriteLine($"Value: {settler.Value}");
+            Console.WriteLine("");
         }
     }
 }
